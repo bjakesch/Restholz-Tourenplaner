90b8b9b569acb289c0cfc0accc140cc5b00c3e74
bjakesch <bennijakesch@gmail.com>
2026-08-11 15:51:38 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,24 +3,19 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 def init_db():
-    # Prüfen, ob Firebase schon initialisiert wurde (verhindert Abstürze)
     if not firebase_admin._apps:
         key_dict = dict(st.secrets["firebase"])
         cred = credentials.Certificate(key_dict)
         firebase_admin.initialize_app(cred)
-    
     return firestore.client()
 
 def load_app_state():
     db = init_db()
-    # Dokument aus Firestore abrufen
     doc = db.collection("config").document("app_state").get()
-    
     if doc.exists:
         return doc.to_dict()
     return {}
 
 def save_app_state(data):
     db = init_db()
-    # Dokument in Firestore schreiben (erstellt es automatisch, falls es nicht existiert)
     db.collection("config").document("app_state").set(data)

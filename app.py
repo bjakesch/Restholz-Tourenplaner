2544577210378178e68c25911219b053ece86ae0
bjakesch <bennijakesch@gmail.com>
2026-08-11 12:55:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import datetime
 # IMPORTE FÜR SPÄTER (Module, die wir noch bauen)
 # ==========================================
  import database as db
-# import logistics as log
+ import logistics as log
 
 # Seitenkonfiguration (muss als erstes aufgerufen werden)
 st.set_page_config(
